de793803fdad984224b204ae028077b1afd02769
Christoffer Grännby <cribbster1987@hotmail.com>
2020-11-16 15:30:49 +0100

    Adding game

---
+++ src/Game/Game.java
@@ -0,0 +1,32 @@
+package Game;
+
+import Config.Player;
+import java.net.Socket;
+
+/**
+ * Created by Christoffer Grännby
+ * Date: 2020-11-15
+ * Time: 12:31
+ * Project: Quizkampen
+ * Copyright: MIT
+ */
+public class Game implements Runnable{
+
+    private Player playerOne;
+    private Player playerTwo;
+
+    private int countingRounds = 1;
+    private int numberOfQuestions;
+    private int totalGames;
+
+    public Game (Socket playerOne, Socket playerTwo){
+          this.playerOne = new Player(playerOne);
+          this.playerTwo = new Player(playerTwo);
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
