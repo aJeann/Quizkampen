9df47ee11bdbbc576c2906f4a4b16a11408be8bf
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2020-11-19 15:41:54 +0100

    I try to fix Client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,10 @@
 package Client;
 
+import UserInterface.GUI;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -34,67 +39,67 @@ public class Client implements Runnable {
     public void run() {
 
     }
-}
 
-//    public void play() throws Exception {
-//        socket = new Socket(LOCATION, PORT);
-//        BufferedReader in = new BufferedReader(new InputStreamReader(
-//                socket.getInputStream()));
-//        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-//
-//        String response;
-//        GUI game = new GUI();
-//        char mark = 'S';
-//        char opponentMark = 'P';
-//        try {
-//            response = in.readLine();
-//            if (response.startsWith("WELCOME")) {
-//                mark = response.charAt(8);
-//                opponentMark = (mark == 'X' ? 'O' : 'X');
-//                frame.setTitle("Tic Tac Toe - Player " + mark);
-//            }
-//            while (true) {
-//                response = in.readLine();
-//                if (response.startsWith("VALID_MOVE")) {
-//                    messageLabel.setText("Valid move, please wait");
-//                    currentSquare.setText(String.valueOf(mark));
-//                    currentSquare.repaint();
-//                } else if (response.startsWith("OPPONENT_MOVED")) {
-//                    int loc = Integer.parseInt(response.substring(15));
-//                    board[loc].setText(String.valueOf(opponentMark));
-//                    board[loc].repaint();
-//                    messageLabel.setText("Opponent moved, your turn");
-//                } else if (response.startsWith("VICTORY")) {
-//                    messageLabel.setText("You win");
-//                    break;
-//                } else if (response.startsWith("DEFEAT")) {
-//                    messageLabel.setText("You lose");
-//                    break;
-//                } else if (response.startsWith("TIE")) {
-//                    messageLabel.setText("You tied");
-//                    break;
-//                } else if (response.startsWith("MESSAGE")) {
-//                    messageLabel.setText(response.substring(8));
-//                }
-//            }
-//            out.println("QUIT");
-//        }
-//        finally {
-//            socket.close();
-//        }
-//    }
-//
-//
-//    public static void main(String[] args) throws Exception {
-//        while (true) {
-//            String serverAddress = (args.length == 0) ? "localhost" : args[1];
-//            Client client = new Client(serverAddress);
-////            client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-////            client.frame.setSize(240, 160);
-////            client.frame.setVisible(true);
-////            client.frame.setResizable(true);
-//            client.play();
-//            if (!client.wantsToPlayAgain()) {
-//                break;
-//            }
-//}
+
+    public void play() throws Exception {
+        socket = new Socket(LOCATION, PORT);
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        String response;
+        GUI game = new GUI();
+        char mark = 'S';
+        char opponentMark = 'P';
+        try {
+            response = in.readLine();
+            if (response.startsWith("WELCOME")) {
+                mark = response.charAt(8);
+                opponentMark = (mark == 'X' ? 'O' : 'X');
+                frame.setTitle("Tic Tac Toe - Player " + mark);
+            }
+            while (true) {
+                response = in.readLine();
+                if (response.startsWith("VALID_MOVE")) {
+                    messageLabel.setText("Valid move, please wait");
+                    currentSquare.setText(String.valueOf(mark));
+                    currentSquare.repaint();
+                } else if (response.startsWith("OPPONENT_MOVED")) {
+                    int loc = Integer.parseInt(response.substring(15));
+                    board[loc].setText(String.valueOf(opponentMark));
+                    board[loc].repaint();
+                    messageLabel.setText("Opponent moved, your turn");
+                } else if (response.startsWith("VICTORY")) {
+                    messageLabel.setText("You win");
+                    break;
+                } else if (response.startsWith("DEFEAT")) {
+                    messageLabel.setText("You lose");
+                    break;
+                } else if (response.startsWith("TIE")) {
+                    messageLabel.setText("You tied");
+                    break;
+                } else if (response.startsWith("MESSAGE")) {
+                    messageLabel.setText(response.substring(8));
+                }
+            }
+            out.println("QUIT");
+        }
+        finally {
+            socket.close();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            String serverAddress = (args.length == 0) ? "localhost" : args[1];
+            Client client = new Client(serverAddress);
+//            client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            client.frame.setSize(240, 160);
+//            client.frame.setVisible(true);
+//            client.frame.setResizable(true);
+            client.play();
+            if (!client.wantsToPlayAgain()) {
+                break;
+            }
+}
