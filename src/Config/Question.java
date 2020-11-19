9e7b4de46ac1e788de83605922fc989f5da17dea
Sako05 <salem-k@hotmail.com>
2020-11-19 22:45:21 +0100

    Added getters and setters

---
+++ src/Config/Question.java
@@ -0,0 +1,49 @@
+package Config;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Question implements Serializable {
+    private String category;
+    private String question;
+    private String correctanswear;
+    private String[] answers;
+
+    public Question(String category, String question, String correctanswear, String[] answers) {
+        this.category = category;
+        this.question = question;
+        this.correctanswear = correctanswear;
+        this.answers = answers;
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public String[] getAnswers(){
+        return answers;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public String getCorrectanswear(){
+        return correctanswear;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
