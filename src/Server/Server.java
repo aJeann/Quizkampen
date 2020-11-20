dc3f1e95fd13e111b2bb4b627e9b74b6b08af511
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2020-11-20 10:47:01 +0100

    I try to fix Client

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -23,6 +23,7 @@ public class Server {
         try (ServerSocket connecting = new ServerSocket(serverPortNumber)){
             while (true){
                 GUI game = new GUI();// change name and call from GUI
+                if(ui.getRoundParam == 1){
                 ServerSidePlayer playerOne = new ServerSidePlayer(connecting.accept(), "NAME1", game);
                 ServerSidePlayer playerTwo = new ServerSidePlayer(connecting.accept(), "MANE2", game);
                 playerOne.setOpponent(playerTwo);
@@ -30,7 +31,10 @@ public class Server {
                 // create currentPlayer
                 // game.currentPlayer = playerOne;
                 playerOne.start();
-                playerTwo.start();
+                playerTwo.start();}else {
+                    ServerSidePlayer playerTwo = new ServerSidePlayer(connecting.accept(), "MANE2", game);
+                    ServerSidePlayer playerOne = new ServerSidePlayer(connecting.accept(), "NAME1", game);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
