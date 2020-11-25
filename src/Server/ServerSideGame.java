99f5b266fa2b0022908ec91d83856a2bb34b5548
Christoffer Grännby <cribbster1987@hotmail.com>
2020-11-25 14:01:17 +0100

    Added methods to ServerSideGame

--- src/Server/ServerSideGame.java
+++ src/Server/ServerSideGame.java
@@ -1,5 +1,6 @@
 package Server;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,54 @@ import java.util.List;
  */
 public class ServerSideGame {
 
+        private GameDB database = new GameDB();
+        private ServerSidePlayer currentPlayer;
+        int questionsPerRound;
+        private int totalRounds;
+        private int currentRound;
 
+        ServerSideGame (int questionsPerRound, int totalRounds){
+            this.questionsPerRound = questionsPerRound;
+            this. totalRounds = totalRounds;
+        }
+
+        void setPlayer (ServerSidePlayer playerOne, ServerSidePlayer playerTwo) {
+            playerOne.setOpponent(playerTwo);
+            playerTwo.setOpponent(playerOne);
+            currentPlayer = playerOne;
+        }
+
+        private ServerSidePlayer getPlayerOne() {
+            if (currentPlayer.getName().equalsIgnoreCase("Player 1")) {
+                return currentPlayer;
+            } else {
+                return currentPlayer.getOpponent();
+            }
+        }
+
+        private ServerSidePlayer getPlayerTwo(){
+            return getPlayerOne().getOpponent();
+        }
+
+        private void winner () throws IOException {
+            if (gameIsOver()) {
+                if (currentPlayer.totalPoints > currentPlayer.getOpponent().totalPoints) {
+                    currentPlayer.outputObject.writeObject("You win!");
+                    currentPlayer.getOpponent().outputObject.writeObject("You lose!");
+                }
+                else if (currentPlayer.totalPoints < currentPlayer.getOpponent().totalPoints){
+                    currentPlayer.outputObject.writeObject("You lose!");
+                    currentPlayer.getOpponent().outputObject.writeObject("You win!");
+                }
+                else {
+                    currentPlayer.outputObject.writeObject("You tied!");
+                    currentPlayer.getOpponent().outputObject.writeObject("You tied!");
+                }
+            }
+        }
+        private boolean gameIsOver(){
+            return currentRound == totalRounds;
+        }
 
 
     ServerSidePlayer currentPlayer;
