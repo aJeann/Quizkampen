fa6d142220ebf3c2f7bdfdbb54cdda136083395d
Christoffer Grännby <cribbster1987@hotmail.com>
2020-11-25 19:38:42 +0100

    Changed String-array to List<String> in Question-class. Added List instead of String-array to GameDB. Added Collection.shuffle for answers and made a main with som system.outs to try the result Started with some changes in text-file.

--- src/Server/GameDB.java
+++ src/Server/GameDB.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import Config.Question;
@@ -17,10 +18,10 @@ import Config.Question;
 public class GameDB {
 
     private final ArrayList<Question> DBquestions = new ArrayList<>();
-/*    private final ArrayList<Question> DBteknologi = new ArrayList<>();
+    private final ArrayList<Question> DBteknologi = new ArrayList<>();
     private final ArrayList<Question> DBsamhälle = new ArrayList<>();
     private final ArrayList<Question> DBmänniskan = new ArrayList<>();
-    private final ArrayList<Question> DBdatorerointernet = new ArrayList<>();*/
+    private final ArrayList<Question> DBdatorerointernet = new ArrayList<>();
 
 
 
@@ -28,20 +29,29 @@ public class GameDB {
     public void GameDBquestions() {
 
         String GameDBquestions;
-        String GameDBanswears;
         String GameDBcategory;
 
+        //TODO fixa svaren i textfilen
         String questionList = "src/Server/QuestionsList.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(questionList))){
 
             while((GameDBcategory = reader.readLine()) != null){
                 GameDBquestions = reader.readLine();
-                GameDBanswears = reader.readLine();
-                String[] AnswersArray = GameDBanswears.split(",");
-                DBquestions.add(new Question(GameDBcategory, GameDBquestions ,AnswersArray[0], new String[]{AnswersArray[1],AnswersArray[2],AnswersArray[3]}));
+                List<String>answers = new ArrayList<>();
+                String correctAnswer = reader.readLine();
+                String answerTwo = reader.readLine();
+                String answerThree = reader.readLine();
+                String answerFour = reader.readLine();
+                answers.add(correctAnswer);
+                answers.add(answerTwo);
+                answers.add(answerThree);
+                answers.add(answerFour);
 
+                Collections.shuffle(answers);
+                //String[] AnswersArray = GameDBanswears.split(",");
+                DBquestions.add(new Question(GameDBcategory, GameDBquestions ,correctAnswer, answers));
 
-                /*
+/*
                Question question = new Question(GameDBcategory, GameDBquestions ,AnswersArray[0], new String[]{AnswersArray[1],AnswersArray[2],AnswersArray[3]});
 
                  if (GameDBcategory.equals("Teknologi")) {
@@ -67,5 +77,16 @@ public class GameDB {
 
     }
 
-
+    public static void main(String[] args) {
+        GameDB database = new GameDB();
+        database.GameDBquestions();
+        for (Question q:database.DBquestions) {
+            System.out.println("Kategori: " + q.getCategory());
+            System.out.println("Fråga: " + q.getQuestion());
+            System.out.println("Rätt svar: " + q.getCorrectanswear());
+            for (String s:q.getAnswers()) {
+                System.out.println("Svarsalternativ: " + s);
+            }
+        }
+    }
 }
